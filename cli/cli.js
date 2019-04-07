bdcd36169648872eadc9b028e6a0e31d00dea0dc
mattb <mattbechtel123@gmail.com>
2019-04-06 20:41:39 -0500

    Added comments to cli.js
        Added some comments descibing each function.
        Also made some error statements more precise.

--- cli/cli.js
+++ cli/cli.js
@@ -1,5 +1,15 @@
 const logger = require('logger');
 
+/** 
+ * Constructor for the CLI object. The program name should be passed as the 
+ * first parameter, and the cliMap as the second. The cliMap should be a 
+ * map of command strings to their runners (functions to be executed if the matching command
+ * is found). 
+ * @author    Matt Bechtel | mbechtel@iastate.edu 
+ * @date      2019-04-06 20:33:27 
+ * @param     {String}     | name of the program using this CLI object
+ * @param     {Map<String, Function} | map of command strings and their corresponding runners (functions)  
+ */
 function CLI(programName, cliMap)
 {
     if(!programName)
@@ -18,20 +28,44 @@ function CLI(programName, cliMap)
     this.cliMap = cliMap;
 }
 
-//TODO Add splitting by space so that runnables are given an array of input strings in order
+/** 
+ * Takes input string and parses it into a string array by splitting the original input string by spaces.
+ * Once the input is parsed, this function will attempt to execute the given 'runner' for the 
+ * first 'command' (this will be the first string in the parsed input string[]). If the runner exists, 
+ * it will be called, passing the parsed input string[] as the arguments and this function will return true. 
+ * If no runner exists, or the original input is null, empty, or undefined, false will be returned. 
+ * @author    Matt Bechtel | mbechtel@iastate.edu 
+ * @date      2019-04-06 20:27:16 
+ * @param     {String} input 
+ * @return    {Boolean}    | true if runner exists, false if not
+ */
 CLI.prototype.parseAndRun = function(input)
 {
-    if(input && this.cliMap[input])
+    let parsedInput = input.split(" "); 
+    let runner = this.cliMap[parsedInput[0]];
+
+    if(parsedInput.length > 0 && runner)
     {
         logger.info("Parsing input and executing command", __filename);
-        this.cliMap[input](input);
+        runner(parsedInput);
+        return true;
     }
-    else 
+    else if(parsedInput.length == 0)
     {
-        logger.error("Input is empty or there is no runnable for it", __filename);
+        logger.error("Input is empty, null, or undefined", __filename);
+        return false;
+    }
+    else if(!runner)
+    {
+        logger.error("Input has no corresponding runner", __filename);
+        return false;
     }
 }
 
+
+/* EXAMPLE CODE */
+
+/*
 var helpRunnerExample = (input) =>
 {
     logger.info("Helper input: " + input, __filename);
@@ -41,5 +75,6 @@ var cliMap = {};
 cliMap["--help"] = helpRunnerExample;
 
 const cli = new CLI("s-chat", cliMap);
-cli.parseAndRun("--help");
+cli.parseAndRun("--help this that");
 logger.info(cli);
+*/
