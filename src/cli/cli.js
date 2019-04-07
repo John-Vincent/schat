10f5988f9d08eceed36745ffa5cc4fc17dbbf04f
mattb <mattbechtel123@gmail.com>
2019-04-06 21:56:19 -0500

    Moved packages into src folder, added cli functionality
        Moved all of the packages, logger, cli, and schat, into an
        src folder. Added functionality and comments to the cli module,
        allowing for simpler usage. Now, to use the cli, one simply
        creates a map object, with the command string as a key
        and the runner as the value, and passes that to the cli on
        instantiation. Then a call to cli.start() will kick off the
        input reading from stdin, and will use the given runners
        (cliMap) for each command received from the user.
    
        Setup the cli with schat.js, formatting the file and stubbing
        some runners that we will need to implement.

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -1,4 +1,5 @@
 const logger = require('logger');
+const readline = require('readline');
 
 /** 
  * Constructor for the CLI object. The program name should be passed as the 
@@ -10,7 +11,7 @@ const logger = require('logger');
  * @param     {String}     | name of the program using this CLI object
  * @param     {Map<String, Function} | map of command strings and their corresponding runners (functions)  
  */
-function CLI(programName, cliMap)
+function CLI(programName, prompt, cliMap)
 {
     if(!programName)
     {
@@ -26,6 +27,12 @@ function CLI(programName, cliMap)
 
     this.programName = programName;
     this.cliMap = cliMap;
+    this.prompt = prompt ? prompt : this.programName + " > ";
+    this.io = readline.createInterface(
+        {
+            input : process.stdin,
+            output : process.stdout
+        });
 }
 
 /** 
@@ -42,7 +49,7 @@ function CLI(programName, cliMap)
 CLI.prototype.parseAndRun = function(input)
 {
     let parsedInput = input.split(" "); 
-    let runner = this.cliMap[parsedInput[0]];
+    let runner = this.cliMap.get(parsedInput[0]);
 
     if(parsedInput.length > 0 && runner)
     {
@@ -58,10 +65,43 @@ CLI.prototype.parseAndRun = function(input)
     else if(!runner)
     {
         logger.error("Input has no corresponding runner", __filename);
+        this.print(parsedInput[0] + " is not a command, try --help");
         return false;
     }
 }
 
+/** 
+ * Kicks off the cli. Writes prompt to stdout, and listens on stdin for user input.
+ * @author    Matt Bechtel | mbechtel@iastate.edu 
+ * @date      2019-04-06 21:34:24    
+ */
+CLI.prototype.start = function()
+{
+    this.io.setPrompt(this.prompt);
+    this.io.prompt();
+    
+    this.io.on('line', (line) => 
+    {
+        logger.info("Read input: " + line, __filename);
+        this.parseAndRun(line);
+        this.io.prompt();
+    });
+}
+
+/** 
+ * Simply prints the given string, plus a new line, to the console. 
+ * It is best to wrap it in this function when using the 'CLI', as 
+ * it is the only way to print the prompt to the console after 
+ * the given string is printed. 
+ * @author    Matt Bechtel | mbechtel@iastate.edu 
+ * @date      2019-04-06 21:52:36 
+ * @param     {String} str | string to be printed to the console
+ */
+CLI.prototype.print = function(str)
+{
+    console.log(str + "\n");
+    this.io.prompt();
+}
 
 /* EXAMPLE CODE */
 
@@ -76,5 +116,6 @@ cliMap["--help"] = helpRunnerExample;
 
 const cli = new CLI("s-chat", cliMap);
 cli.parseAndRun("--help this that");
-logger.info(cli);
 */
+
+module.exports = CLI;
---
+++ src/schat/schat.js
@@ -0,0 +1,48 @@
+const readline = require('readline');
+const logger = require('logger');
+const CLI = require('cli');
+
+/* RUNNERS */
+
+/** 
+ * TODO
+ * Runner for the 'add user to chat' function of schat. 
+ * @author    Matt Bechtel | mbechtel@iastate.edu 
+ * @date      2019-04-06 21:28:31 
+ * @param     {String[]} input | parsed input, placed into string[]
+ */
+const addRunner = function(input)
+{
+    logger.info("Add runner input: " + input, __filename);
+
+    //here should be the logic for adding a user to the chat
+    cli.print("Added user to chat");
+}
+
+/** 
+ * TODO
+ * Runner for the 'help' command. This should print help information to the 
+ * console. Using cli.print(str) is advised, as it is the only way to have the prompt 
+ * print after the help information is displayed. 
+ * @author    Matt Bechtel | mbechtel@iastate.edu 
+ * @date      2019-04-06 21:49:55 
+ * @param     {String[]} input | parsed input, place into string[]
+ */
+const helpRunner = function(input)
+{
+    logger.info("Help runner input: " + input);
+
+    //logic for help command    
+    cli.print("Help information");
+}
+
+/*set up the 'CLI'*/
+let cliMap = new Map();
+let programName = "schat";
+let prompt = programName + " > ";
+cliMap.set("add", addRunner);
+cliMap.set("--help", helpRunner);
+let cli = new CLI(programName, prompt, cliMap);
+
+/*start the cli (begin reading input from stdin)*/
+cli.start();
